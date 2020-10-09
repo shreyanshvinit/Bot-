cc771cde5c3e66d2e05714a91d10b7d5d94a9201
Shreyansh Vinit <shreyanshvinit@gmail.com>
2020-10-09 16:23:13 +0530

    Checkpoint 1 error

---
+++ index.js
@@ -0,0 +1,46 @@
+const Telegraf = require('telegraf')
+const axios = require('axios')
+require('dotenv').config()
+
+const bot = new Telegraf('process.env.TOKEN_TESTBOT')
+console.log(process.env.TOKEN_TESTBOT)
+
+bot.start((ctx) => {
+    ctx.reply("The bot has started!")
+})
+
+bot.help((ctx) => {
+    ctx.reply("I am always ready to help u! Type '/jobs' to know the task performed by me.")
+})
+
+bot.command('jobs', (ctx)=>{
+    ctx.reply =( `I can :
+    1. Tell fortune (/fortune)
+    2. Give info. about dev-talks (/DevTalks)
+    3. Display github actions (/ghactions)` )
+})
+
+bot.command('fortune', (ctx)=>{
+    url = "http://yerkee.com/api/fortune"
+    axios.get(url)
+    .then((res)=>{
+        ctx.reply(res.data.fortune)
+    })
+})
+
+bot.command('DevTalks', ctx => {
+    
+    url = "https://api.github.com/repos/COPS-IITBHU/DevTalks/issues"
+    axios.get(url)
+    .then(res => {
+        if(res.data.length == 0) {
+            ctx.reply('No Dev-Talks scheduled');
+        }
+        console.log(res.data)
+    })
+})
+
+bot.launch();
+
+
+
